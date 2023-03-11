9b875734aaba345134c10c66543a034b2e88b58b
amlaksil <silamlakdesye@gmail.com>
2023-03-11 19:00:14 +0300

    Handle basic math operation

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from sys import argv, exit
+from calculator_1 import add, sub, mul, div
+
+
+def basic_operation():
+    n = len(argv)
+    if n - 1 != 3:
+        print("Usage: {} {} {} {}".format(argv[0], argv[1], argv[2], argv[3]))
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == '+':
+        print("{} {} {} = {}".format(a, argv[2], b, add(a, b)))
+    elif argv[2] == '-':
+        print("{} {} {} = {}".format(a, argv[2], b, sub(a, b)))
+    elif argv[2] == '*':
+        print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
+    elif argv[2] == '/':
+        print("{} {} {} = {}".format(a, argv[2], b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
+if __name__ == "__main__":
+    basic_operation()
