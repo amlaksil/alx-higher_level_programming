240c3357ed9c7107f772e19b693e19f34df87365
amlaksil <silamlakdesye@gmail.com>
2023-03-14 17:00:24 +0300

    Checks if a list is palindrom

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,26 +9,92 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr;
-	int k = 0, len = 0;
-	int a[1024];
+	listint_t *current = *head, *prev = *head, *slow = *head, *fast = *head;
+	listint_t *midnode = NULL, *second_half;
+	int status;
 
-	if (*head == NULL)
+	if (current == NULL && current->next == NULL)
 		return (1);
-	ptr = *head;
-	while (ptr != NULL)
+	/*
+	 * Get the middle of the list. Move slow by 1 and  fast by 2 slow will
+	 * have the middle node
+	*/
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		a[len] = ptr->n;
-		ptr = ptr->next;
-		len++;
+		fast = fast->next->next;
+		prev = slow, slow = slow->next;
 	}
-	while (a[k] == a[len - 1])
+	/*
+	 * fast would become NULL for enven number node and for odd we need
+	 * to skip the middle node and store it somewhere so that we can restore
+	 * the original list
+	*/
+	if (fast != NULL)
+		midnode = slow, slow = slow->next;
+
+	second_half = slow, prev->next = NULL; /* terminate first half */
+	reverse(&second_half);
+	status = compare_list(current, second_half);
+	/* Restore the original list */
+	reverse(&second_half);
+	if (midnode != NULL)
+		prev->next = midnode, midnode->next = second_half;
+	else
+		prev->next = second_half;
+
+	if (status != 0)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * reverse - listint_t singlylinked list
+ * @head: pointer to pointer which points to the first item in the list
+ *
+ * Return: void
+ */
+void reverse(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *prev = NULL, *temp;
+
+	while (current != NULL)
 	{
-		k++;
-		len--;
-		if (k == len)
-			return (1);
-		continue;
+		temp = current->next;
+		current->next = prev;
+		prev = current;
+		current = temp;
 	}
+	*head = prev;
+}
+
+/**
+ * compare_list - compares the two list given
+ * @head1: pointer to the first list
+ * @head2: pointer to the second list
+ *
+ * Return: 0 if the list contain the same element if not 1
+ */
+int compare_list(listint_t *head1, listint_t *head2)
+{
+	listint_t *current = head1;
+	listint_t *second_half = head2;
+
+	while (current && second_half)
+	{
+		if (current->n == second_half->n)
+		{
+			current = current->next;
+			second_half = second_half->next;
+		}
+		else
+			return (0);
+	}
+	if (current == NULL && second_half == NULL)
+		return (1);
+
+       /* will reach here when one is NULL and other is not */
 	return (0);
 }
