193da08f91b30d6a642875ea631a478d4f2577aa
amlaksil <silamlakdesye@gmail.com>
2023-03-16 02:40:31 +0300

    Replace an element by another

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    n = len(my_list)
-    for i in range(n):
-        if my_list[i] == search:
-            my_list[i] = replace
-            new_list = my_list[:i] + my_list[i:]
-            my_list[i] = search
-        else:
-            new_list = my_list
-    return new_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
