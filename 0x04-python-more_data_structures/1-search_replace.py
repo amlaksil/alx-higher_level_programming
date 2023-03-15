934f008a6aa9ed2724f4db747b60ad5d30468e53
amlaksil <silamlakdesye@gmail.com>
2023-03-16 01:41:03 +0300

    Replace element in a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    n = len(my_list)
+    if search not in my_list:
+        return my_list
+    for i in range(n):
+        if my_list[i] == search:
+            my_list[i] = replace
+            new_list = my_list[:i] + my_list[i:]
+            my_list[i] = search
+            return new_list
