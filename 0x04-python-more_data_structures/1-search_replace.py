fdc9f0522a2148e5e04fdc4bfac3001ab7e8cf4a
amlaksil <silamlakdesye@gmail.com>
2023-03-16 02:01:25 +0300

    Use buitin method

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     n = len(my_list)
-    if search not in my_list:
-        return my_list
+    new_list = my_list.copy()
     for i in range(n):
         if my_list[i] == search:
-            my_list[i] = replace
-            new_list = my_list[:i] + my_list[i:]
-            my_list[i] = search
+            new_list.insert(i, replace)
             return new_list
