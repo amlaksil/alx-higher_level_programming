a5b22185b62151e22202a2665d1acb29a892cfa0
amlaksil <silamlakdesye@gmail.com>
2023-03-15 04:29:18 +0300

    Remove character at given position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0 or n > len(str) - 1:
+        return str
+    for i in range(len(str)):
+        if i == n:
+            str = str[: i] + str[i + 1:]
+            return str
