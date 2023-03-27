5c4f45bbea7c11491255ee920f0ff6e23d699323
amlaksil <silamlakdesye@gmail.com>
2023-03-27 18:52:50 +0300

    Cast before print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        y = int(value)
+        print("{:d}".format(y))
         return True
     except ValueError:
         return False
