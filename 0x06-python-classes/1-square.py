fdf6844b53ed9a471ce7407d5c02bd8401f4133b
amlaksil <silamlakdesye@gmail.com>
2023-03-29 22:18:01 +0300

    Define the size of square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class based on 0-square.py"""
+
+
+class Square:
+    """square class that defines size of the square (square object)"""
+
+
+    def __init__(self, size):
+        """Initializing this square class
+
+        Args:
+            size: represents the size of the square defined
+        """
+        self.__size = size
