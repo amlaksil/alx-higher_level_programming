677270df2a24395dae70a8072269869cd84a1bae
amlaksil <silamlakdesye@gmail.com>
2023-03-30 03:45:59 +0300

    Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""A module that defines a square """
+
+
+class Square:
+    """square class that defines size and calculate the area of square """
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
+
+    def __init__(self, size=0):
+        """Initializing square class
+
+        Args:
+            size (int): the size of the square
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """This getter method retrieves the size and the setter method sest
+        the right value for size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the area of square which is the square of its length """
+        return int(self.__size) ** 2
