3f69bc7a5b669ce348be0a66f00584c62d5853bd
amlaksil <silamlakdesye@gmail.com>
2023-04-03 19:18:18 +0300

    Adjust indentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,41 +11,43 @@ class Rectangle:
             width (int): width of the rectangle
             height (int): height of the rectangle
         """
-        self.height = height
         self.width = width
+        self.height = height
 
-        @property
-        def width(self):
-            """This getter method retrieve the width of the rectangle and
-            the setter sets the right value for width
-            Raises:
-                TypeError: if the given width is not integer
-                ValueError: if the given width less than zero
-            """
-            return self.__width
+    @property
+    def width(self):
+        """This getter method retrieve the width of the rectangle and
+        the setter sets the right value for width
+        Raises:
+            TypeError: if the width is not integer
+            ValueError: if the width less than zero
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
             self.__width = value
 
-        @property
-        def height(self):
-            """This getter method retrieve the height of the rectangle and
-            the setter sets the right value for the height
-            Raises:
-                TypeError: if the height is not integer
-                ValueError: if the height less than zero
-            """
-            return self.__height
+    @property
+    def height(self):
+        """This getter method retrieve the height of the rectangle and
+        the setter sets the right value for the height
+        Raises:
+            TypeError: if the height is not integer
+            ValueError: if the height less than zero
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
             self.__height = value
