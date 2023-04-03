1b8b71952e5566692bd614a740d8432a70004f52
amlaksil <silamlakdesye@gmail.com>
2023-04-03 17:51:00 +0300

    Create two instance attribute

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This module defines Rectangle class"""
+
+
+class Rectangle:
+    """A class that defines width and height of the rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializing the width and height of the rectangle.
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.__height = height
+        self.__width = width
+
+        @property
+        def width(self):
+            """This getter method retrieve the width of the rectangle and
+            the setter sets the right value for width
+            Raises:
+                TypeError: if the given width is not integer
+                ValueError: if the given width less than zero
+            """
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        @property
+        def height(self):
+            """This getter method retrieve the height of the rectangle and
+            the setter sets the right value for the height
+            Raises:
+                TypeError: if the height is not integer
+                ValueError: if the height less than zero
+            """
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
