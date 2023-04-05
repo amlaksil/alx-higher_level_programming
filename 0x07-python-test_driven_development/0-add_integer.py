bcc4bd7880fd51048bd10a12acfd4327a39639d7
amlaksil <silamlakdesye@gmail.com>
2023-04-05 23:05:54 +0300

    Add two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A simple module that contains only one
+function ``add_integer()`` that adds two
+numbers if the number are float or int otherwise
+it will raises an error.
+"""
+def add_integer(a, b=98):
+    """Adds two integer number and retuns the result
+    Args:
+        a (int): the first number
+        b (int): second number
+    Raises:
+        TypeError: if the numbers are not integer or float
+    Return:
+        the sum of the two number
+    """
+    if type(a) is None or type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
