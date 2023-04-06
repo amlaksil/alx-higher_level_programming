57aa8b26bc9898baa6d10cf0805bc6a6e3a8eb08
amlaksil <silamlakdesye@gmail.com>
2023-04-06 16:30:18 +0300

    Take a given character as delimeter and print text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module consists of a single function called
+``text_indentation`` which prints a text with 2 new lines
+after each of these characters `.`, `?` and `:`
+
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters:
+    `.`, `?` and `:`
+    Args:
+        text (str): a text to be sliced
+    Raises:
+        TypeError: if the text is not string
+    """
+    i = 1
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
+    for char in text:
+        if char == "." or char == "?":
+            print(char)
+            print()
+            i = 0
+            continue
+        if char == ":":
+            print(char)
+            i = 0
+            print()
+            continue
+
+        print("{}".format(char if i != 0 else char.strip()), end='')
+        i = 1
