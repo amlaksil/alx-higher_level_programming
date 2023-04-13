af7a87ac22da3d0fea09f51f8e194ea4582bd363
amlaksil <silamlakdesye@gmail.com>
2023-04-14 00:05:44 +0300

    Insert a line of text after specific string

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains a function called
+`append_after` that inserts a line of text
+to a file, after each line containing a specific
+string
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file after specific string
+    Args:
+        filename (str): the name of a file
+        search_string (str): string to be searched
+        new_string (str): a string to be added after search_string
+    """
+    position = 0
+    with open(filename, 'r+', encoding="utf-8") as f:
+        contents = f.readlines()
+        for line in contents:
+            if search_string in line:
+                contents.insert(position + 1, new_string)
+            position += 1
+        '''set the reference point(pointer) at the beginning of the file
+        or open a file again in write mode to trancate it  '''
+        f.seek(0)
+        f.truncate()  # Clear previous content
+        f.writelines(contents)
