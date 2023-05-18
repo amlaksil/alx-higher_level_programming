484ed800c1d2925f7d140d0c6527c3ce3b623f2a
amlaksil <silamlakdesye@gmail.com>
2023-05-18 17:43:18 +0300

    Prints full description of the table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints the full description a table `first_table` from the
+-- database `hbtn_0c_0` in your MySQL server
+-- the first nonoption argument is taken as the name of default database
+SHOW CREATE TABLE first_table;
