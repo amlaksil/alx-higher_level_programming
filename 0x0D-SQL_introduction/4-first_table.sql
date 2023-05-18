0d675659172f80f065d71d56265b3618f7bbf9b9
amlaksil <silamlakdesye@gmail.com>
2023-05-18 17:05:29 +0300

    Create a table named first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates a table named `first_table` in the current database
+-- the first nonoption argument is taken as the name of default database
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
