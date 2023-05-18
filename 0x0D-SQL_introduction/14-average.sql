6f5a7e1764f5c9a98171e9244fd62a0489486c86
amlaksil <silamlakdesye@gmail.com>
2023-05-18 22:23:14 +0300

    Compute the score average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- computes the score average of all records in the table `second_table`
+-- the first nonoption argument is taken as the name of default database
+SELECT AVG(score) AS average FROM second_table;
