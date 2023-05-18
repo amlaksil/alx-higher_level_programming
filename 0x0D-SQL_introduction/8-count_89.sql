9a84ebd346c7ebfa6108927eb328bfccb374bce1
amlaksil <silamlakdesye@gmail.com>
2023-05-18 18:18:41 +0300

    Print the number of records

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with id = 89 in the table `first_table`
+-- the first nonoption argument is taken as the name of default database
+SELECT COUNT(*) FROM first_table WHERE id = 89;
