4223b2ff9f8ea8528d69aef9d3e0c367d4a22710
amlaksil <silamlakdesye@gmail.com>
2023-05-18 18:17:26 +0300

    Insert a new row in the table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- inserts a new row in the table `first_table` form database `hbtn_0c_0`
+-- the first nonoption argument is taken as the name of default database
+INSERT INTO first_table VALUES(89, 'Best School');
