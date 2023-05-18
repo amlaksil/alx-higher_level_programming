d87e7c62188f1abb6b2b6ad6eb21e5ece72eb0d8
amlaksil <silamlakdesye@gmail.com>
2023-05-18 22:00:26 +0300

    Delete score less than five

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- removes all records with a `score <= 5` in the table `second_table` of the
+-- database `hbtn_0c_0`
+-- the first nonoption argument is taken as the name of default database
+DELETE FROM second_table WHERE score <= 5;
