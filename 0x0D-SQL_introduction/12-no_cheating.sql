f156094777a57dbebb10f3024cd24378435bf480
amlaksil <silamlakdesye@gmail.com>
2023-05-18 21:43:15 +0300

    Update the score of Bob in second_table

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- updates the score of `Bob` to 10 in the table `second_table`
+-- the first nonoption argument is taken as the name of default database
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
