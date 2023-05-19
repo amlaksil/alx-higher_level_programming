3ca14078ac71719f370060ae0c0d765524a2998b
amlaksil <silamlakdesye@gmail.com>
2023-05-19 03:49:55 +0300

    Create a table named force_name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- creates a table named `force_name` on your MySQL server
+-- the first nonoption argument is taken as the name of default database
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
