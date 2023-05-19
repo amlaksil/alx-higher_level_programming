c8378e0116a3b675b770ca2d8ff504de5b809bf4
amlaksil <silamlakdesye@gmail.com>
2023-05-19 04:15:31 +0300

    Add default value

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates a table named `unique_id`
 -- the first nonoption argument is taken as the name of the default database
-CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1, name VARCHAR(256));
