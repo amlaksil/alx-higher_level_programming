688a54fdd75adfdcddcf00b95179f452655bba8f
amlaksil <silamlakdesye@gmail.com>
2023-05-19 05:15:31 +0300

    Create cities table with constraints

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates the database `hbtn_0d_usa` and the table `cities`
+-- cities - id INT unique, auto generated, can't be null and is a primary key
+-- state_id INT, can't be null and must be FOREIGN KEY that references to id of
+-- the `states` table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL
+       PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+       name VARCHAR(256) NOT NULL);
