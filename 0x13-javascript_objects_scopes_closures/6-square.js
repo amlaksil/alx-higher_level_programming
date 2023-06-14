886a14e706a2be6c0b5f62d106356220ff94df84
amlaksil <silamlakdesye@gmail.com>
2023-06-14 03:24:45 +0300

    Fix typo error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareF = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareF {
   constructor (size) {
-    super(size, size);
+    super(size);
     this.size = size;
   }
 
   charPrint (c) {
-    if (c === undefined) this.print();
-    else {
+    if (c === undefined) {
+      this.print();
+    } else {
       for (let i = 0; i < this.size; i++) {
         let s = '';
         for (let j = 0; j < this.size; j++) {
