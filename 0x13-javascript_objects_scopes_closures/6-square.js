bf282fac3d27c0e5c3547c7ecb9c8f99892ebbfb
amlaksil <silamlakdesye@gmail.com>
2023-06-14 04:27:08 +0300

    Redefine square class again

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,22 +2,16 @@
 const SquareF = require('./5-square');
 
 class Square extends SquareF {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        let s = '';
-        for (let j = 0; j < this.size; j++) {
-          s += 'C';
-        }
-        console.log(s);
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
       }
+      console.log(s);
     }
   }
 }
