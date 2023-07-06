6c1af7a891e60e88f7a2f80d9ec14443ec39cf16
amlaksil <silamlakdesye@gmail.com>
2023-07-05 17:20:24 -0700

    Add space to separet Content-Type with its value

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a Json POST request to a URL passed as the first argument and display the body of the response
-curl -s "$1" -d "@$2" -X POST -H "Content-Type:application/json"
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
