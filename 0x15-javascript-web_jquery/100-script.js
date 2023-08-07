a18978745772695624c36a5eb68168046f6ac596
amlaksil <silamlakdesye@gmail.com>
2023-08-07 12:30:34 +0300

    Update text color of <header>

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+  /* Is vanilla JavaScript function equivalent to '.(document).ready()' function in JQuery. Executes when the DOM(Document Object Model) is ready. */
+  document.querySelector('header').style.color = '#FF0000';
+};
