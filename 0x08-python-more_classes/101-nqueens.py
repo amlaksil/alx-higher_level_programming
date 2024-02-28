954701a6c0bff5660aadd24c594e6036bc29594d
amlaksil <silamlakdesye@gmail.com>
2024-02-28 11:50:44 +0300

    Implement functions that solve the N-queens problem

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+import sys
+
+
+def is_safe(board, row, col):
+    """
+    Check if it is safe to place a queen at a specific position on the board.
+
+    Args:
+        board (list): A list representing the current state of the chessboard.
+        row (int): The row index where the queen is to be placed.
+        col (int): The column index where the queen is to be placed.
+
+    Returns:
+        bool: True if it is safe to place a queen at the given position, False
+    otherwise.
+    """
+    for i in range(col):
+        if board[i] == row or board[i] == row - (col - i) or\
+           board[i] == row + (col - i):
+            return False
+    return True
+
+
+def solve_util(board, col, n, solutions):
+    """
+    Recursive utility function to solve the N-queens problem.
+
+    Args:
+        board (list): A list representing the current state of the chessboard.
+        col (int): The current column being considered for queen placement.
+        n (int): The size of the board and the number of queens to place.
+        solutions (list): A list to store the valid solutions.
+
+    Returns:
+        None
+    """
+    if col >= n:
+        solutions.append(board[:])
+        return
+    for row in range(n):
+        if is_safe(board, row, col):
+            board[col] = row
+            solve_util(board, col + 1, n, solutions)
+
+
+def solve_nqueens(n):
+    """
+    Solve the N-queens problem for a given board size.
+
+    Args:
+        n (int): The size of the board and the number of queens to place.
+
+    Returns:
+        None
+
+    Prints:
+        Every possible solution to the N-queens problem, one solution
+        per line. The format of each solution is a representation of the
+        board configuration.
+
+    Raises:
+        ValueError: If `n` is not an integer.
+        SystemExit: If `n` is less than 4 or the wrong number of arguments
+    is provided.
+    """
+    if len(sys.argv) != 2:
+        print(f'Usage: nqueens {n}')
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print(f'{n} must be a number')
+        sys.exit(1)
+
+    if n < 4:
+        print(f'{n} must be at least 4')
+        sys.exit(1)
+
+    board = [-1] * n
+    solutions = []
+    solve_util(board, 0, n, solutions)
+    for solution in solutions:
+        for i in range(n):
+            print("[{}, {}]".format(i, solution[i]), end=' ')
+        print()
+
+
+if __name__ == '__main__':
+    solve_nqueens(sys.argv[1])
