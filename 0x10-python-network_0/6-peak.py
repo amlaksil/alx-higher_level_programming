84044d2b032d8930333bf27f8da5bebdd6cff1dd
amlaksil <silamlakdesye@gmail.com>
2024-03-14 09:46:45 +0300

    Fix pycodestyle issue

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,28 +5,31 @@ the first peak element (is an element that is strictly greater that
 its neighbors) in the list.
 """
 
+
 def find_peak(list_of_integers):
-	"""Retuns the firs peak element in the list
-		Args:
-			list_of_integers (list): list of unsorted integers
-	"""
-	if list_of_integers == []:
-		return None
+    """
+    Retuns the first peak element in the list.
 
-	length = len(list_of_integers)
-	mid = length // 2
-	li = list_of_integers
+    Args:
+        list_of_integers (list): list of unsorted integers
+    """
+    if list_of_integers == []:
+        return None
 
-	if mid - 1 < 0 and mid + 1 >= length:
-		return li[mid]
-	elif mid - 1 < 0:
-		return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-	elif mid + 1 >= length:
-		return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
+    length = len(list_of_integers)
+    mid = length // 2
+    li = list_of_integers
 
-	if li[mid - 1] < li[mid] > li[mid + 1]:
-		return li[mid]
+    if mid - 1 < 0 and mid + 1 >= length:
+        return li[mid]
+    elif mid - 1 < 0:
+        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
+    elif mid + 1 >= length:
+        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
 
-	if li[mid + 1] > li[mid - 1]:
-		return find_peak(li[mid:])
-	return find_peak(li[:mid])
+    if li[mid - 1] < li[mid] > li[mid + 1]:
+        return li[mid]
+
+    if li[mid + 1] > li[mid - 1]:
+        return find_peak(li[mid:])
+    return find_peak(li[:mid])
